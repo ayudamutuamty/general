9d9eb59c7fd24321d71e13560989eca8983eb2c3
ccastillo_tegik <ccastillo@tegik.com>
2020-04-19 16:06:57 -0500

    remove credencials sendgrud

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,76 @@
 import * as functions from 'firebase-functions';
 import * as sgMail from '@sendgrid/mail';
+import { sendGrid } from './sendgrid';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //  export GOOGLE_APPLICATION_CREDENTIALS="/home/ccastillo/Documents/covid19/functions/covid19-273816-0a4922a6f109.json"
-//echo "export SENDGRID_API_KEY='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'" > sendgrid.env
-
 
 
 export const helloWorld = functions.https.onRequest((request, response) => {
 
+    let contact: any = {
+        body: "asunto",
+        email: "cesar.cast.more@gmail.com",
+        first_name: "Soriana",
+        id: "1hKwNviFsXgXGsYJvdrN",
+        last_name: "soriana",
+        subject: "Asunto"
 
-    sendGoodbyeEmail('ccastillo@tegik.com', 'Hello');
+    }
+
+
+
+    let text: string = contact.email + '\n';
+    text += contact.first_name + ' ' + contact.last_name + '\n';
+    text += contact.body;
+
+    sgMail.setApiKey(sendGrid.api);
+
+    const msg = {
+        from: 'ccastillo@tegik.com',
+        to: 'cesar.cast.more@gmail.com',
+        subject: contact.subject,
+        text: text,
+    };
+
+    console.log("MESSAGE", msg);
+
+    sgMail.send(msg).then((success: any) => {
+        console.log(success);
+    }, error => {
+        console.error(error);
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    });
+
     response.send("Hello from Firebase!");
+
+
 });
 
-// Sends a goodbye email to the given user.
-async function sendGoodbyeEmail(email: string, displayName: string) {
 
-    sgMail.setApiKey('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');
-    const msg = {
-        to: 'cesar.cast.more@gmail.com',
-        from: email,
-        subject: displayName,
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg)
-        .then(() => {}, error => {
-            console.error(error);
+exports.createContact = functions.firestore
+    .document('/contacts/{id}')
+    .onCreate((snap, context) => {
 
-            if (error.response) {
-                console.error(error.response.body)
-            }
-        });
-        
-    console.log('Account deletion confirmation email sent to:', email);
-    return null;
-}
+        let contact: any = snap.data();
+
+
+        let text: string = contact.email + '\n';
+        contact += contact.first_name + ' ' + contact.last_name + '\n';
+        text += contact.body;
+
+        sgMail.setApiKey(sendGrid.api);
+        const msg = {
+            from: 'ccastillo@tegik.com',
+            to: 'cesar.cast.more@tegik.com',
+            subject: contact.subject,
+            text: text,
+            html: '',
+        };
+        return sgMail.send(msg);
+
+    });
